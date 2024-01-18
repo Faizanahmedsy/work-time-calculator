01a2a0e5d86ad1ae8acb014a1b19772793f543ff
Faizanahmed <faizanahmed.devstree@gmail.com>
2024-01-18 16:28:33 +0530

    added notes

--- components/AntProvider.js
+++ components/AntProvider.js
@@ -3,6 +3,7 @@
 import { ConfigProvider, theme } from "antd";
 import React from "react";
 
+//This is the configuration for the antd library
 const AntProvider = ({ children }) => {
   const { defaultAlgorithm, darkAlgorithm } = theme;
   return (
