77a4efb2486e7d7ee642a5fbc302291f5d0c5680
faizanahmed-devstree <faizanahmed.devstree>
2024-07-04 18:48:05 +0530

    feat: added work completed till now

--- app/page.js
+++ app/page.js
@@ -8,17 +8,19 @@ import { QuestionCircleOutlined } from "@ant-design/icons";
 import Link from "next/link";
 
 export default function Home() {
-  const [currentTime, setCurrentTime] = useState("");
+  const [currentTime, setCurrentTime] = useState(dayjs());
   const [completionTime, setCompletionTime] = useState("");
   const [arrivalTime, setArrivalTime] = useState(null);
   const [breakMinutes, setBreakMinutes] = useState(null);
+  const [timeCompleted, setTimeCompleted] = useState(null);
 
   const workTimeInMinutes = 8 * 60 + 15; // Total minutes for 8 hours 15 minutes
 
   useEffect(() => {
     // Update the time every second
     const intervalId = setInterval(() => {
-      setCurrentTime(dayjs().format("hh:mm:ss a"));
+      const now = dayjs();
+      setCurrentTime(now);
     }, 1000);
 
     return () => clearInterval(intervalId);
@@ -27,8 +29,9 @@ export default function Home() {
   useEffect(() => {
     if (arrivalTime && breakMinutes !== null) {
       calculateCompletionTime();
+      calculateTimeCompleted();
     }
-  }, [arrivalTime, breakMinutes]);
+  }, [arrivalTime, breakMinutes, currentTime]);
 
   const calculateCompletionTime = () => {
     if (arrivalTime && breakMinutes !== null) {
@@ -39,11 +42,28 @@ export default function Home() {
     }
   };
 
+  const calculateTimeCompleted = () => {
+    if (arrivalTime && breakMinutes !== null) {
+      const start = dayjs(arrivalTime, "HH:mm");
+      const elapsedMinutes = currentTime.diff(start, "minute") - breakMinutes;
+
+      if (elapsedMinutes <= 0) {
+        setTimeCompleted("0 hours 0 minutes");
+      } else {
+        const completedHours = Math.floor(elapsedMinutes / 60);
+        const completedMinutes = elapsedMinutes % 60;
+        setTimeCompleted(`${completedHours} hours ${completedMinutes} minutes`);
+      }
+    }
+  };
+
   return (
     <Background>
       <div className="flex flex-col h-screen justify-center items-center relative z-10 leading-5 tracking-wider gap-14">
         <div>
-          <h1 className="text-3xl font-bold">{currentTime}</h1>
+          <h1 className="text-3xl font-bold">
+            {currentTime.format("hh:mm:ss a")}
+          </h1>
         </div>
         <div className="flex items-center gap-4 flex-col">
           <div className="font-bold">When did you come to office?</div>
@@ -58,7 +78,7 @@ export default function Home() {
             onChange={(time, timeString) => setArrivalTime(timeString)}
           />
           <div className="font-bold">
-            How many minutes did you took the break?
+            How many minutes did you take for break?
           </div>
 
           <InputNumber
@@ -72,11 +92,14 @@ export default function Home() {
           />
         </div>
 
-        {completionTime && (
+        {completionTime && timeCompleted && (
           <div>
-            <h1 className="text-2xl font-bold">
+            <h1 className="text-2xl text-center font-bold">
               Work Time Completes at: {completionTime}
             </h1>
+            <h3 className="text-md  text-center font-semibold text-zinc-500">
+              Total work time you have completed till now: {timeCompleted}
+            </h3>
           </div>
         )}
 
@@ -98,6 +121,10 @@ export default function Home() {
         >
           Give a Star on Github
         </Link>
+      </div>
+      <div className="absolute bottom-6 right-10 text-zinc-500 text-sm">
+        ⚠️ Note: Please do not blindly trust this tool. <br /> The developer has
+        failed maths 5 times till now
       </div>
     </Background>
   );
